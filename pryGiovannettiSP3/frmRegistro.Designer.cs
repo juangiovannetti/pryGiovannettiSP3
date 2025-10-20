aca06e3babdd0cbc2bce4d0c9d0b0514e9fa5265
juan ignacio giovannetti <giovannettijuan123@gmail.com>
2025-10-19 23:34:35 -0300

    correccion

--- pryGiovannettiSP3/frmRegistro.Designer.cs
+++ pryGiovannettiSP3/frmRegistro.Designer.cs
@@ -28,74 +28,176 @@
         /// </summary>
         private void InitializeComponent()
         {
+            TabPage tpDatos;
+            TabPage Consultas;
             lblMacaDeRespuesto = new Label();
+            btnIngresar = new Button();
             lblOrigenDeRespuesto = new Label();
+            btnCancelar = new Button();
             lblNumeroDelRespuesto = new Label();
+            mtbNumeroRespuesto = new MaskedTextBox();
             lblDescripcion = new Label();
+            txtDescripcion = new TextBox();
             lblPrecioDelRespuesto = new Label();
+            cmbOrigen = new ComboBox();
             lblPrecio = new Label();
             cmbMarca = new ComboBox();
-            cmbOrigen = new ComboBox();
-            txtDescripcion = new TextBox();
-            mtbNumeroRespuesto = new MaskedTextBox();
-            btnCancelar = new Button();
-            btnIngresar = new Button();
+            btnConsultar = new Button();
+            lblMarcaDeRepuesto = new Label();
+            lstDatos = new ListBox();
+            lblOrigenDeRepuesto = new Label();
+            chkImportado = new RadioButton();
+            cmbMarcaCta = new ComboBox();
+            chkNacional = new RadioButton();
+            tpConsultar = new TabControl();
+            tpDatos = new TabPage();
+            Consultas = new TabPage();
+            tpDatos.SuspendLayout();
+            Consultas.SuspendLayout();
+            tpConsultar.SuspendLayout();
             SuspendLayout();
+            // 
+            // tpDatos
+            // 
+            tpDatos.Controls.Add(lblMacaDeRespuesto);
+            tpDatos.Controls.Add(btnIngresar);
+            tpDatos.Controls.Add(lblOrigenDeRespuesto);
+            tpDatos.Controls.Add(btnCancelar);
+            tpDatos.Controls.Add(lblNumeroDelRespuesto);
+            tpDatos.Controls.Add(mtbNumeroRespuesto);
+            tpDatos.Controls.Add(lblDescripcion);
+            tpDatos.Controls.Add(txtDescripcion);
+            tpDatos.Controls.Add(lblPrecioDelRespuesto);
+            tpDatos.Controls.Add(cmbOrigen);
+            tpDatos.Controls.Add(lblPrecio);
+            tpDatos.Controls.Add(cmbMarca);
+            tpDatos.Location = new Point(4, 29);
+            tpDatos.Name = "tpDatos";
+            tpDatos.Padding = new Padding(3);
+            tpDatos.Size = new Size(383, 357);
+            tpDatos.TabIndex = 0;
+            tpDatos.Text = "Datos";
+            tpDatos.UseVisualStyleBackColor = true;
             // 
             // lblMacaDeRespuesto
             // 
             lblMacaDeRespuesto.AutoSize = true;
             lblMacaDeRespuesto.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblMacaDeRespuesto.Location = new Point(11, 76);
+            lblMacaDeRespuesto.Location = new Point(20, 24);
             lblMacaDeRespuesto.Name = "lblMacaDeRespuesto";
             lblMacaDeRespuesto.Size = new Size(166, 23);
             lblMacaDeRespuesto.TabIndex = 0;
             lblMacaDeRespuesto.Text = "Marca De Respuesto";
             // 
+            // btnIngresar
+            // 
+            btnIngresar.BackColor = Color.Gray;
+            btnIngresar.Enabled = false;
+            btnIngresar.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnIngresar.ForeColor = SystemColors.ControlLightLight;
+            btnIngresar.Location = new Point(206, 300);
+            btnIngresar.Margin = new Padding(3, 4, 3, 4);
+            btnIngresar.Name = "btnIngresar";
+            btnIngresar.Size = new Size(106, 44);
+            btnIngresar.TabIndex = 11;
+            btnIngresar.Text = "Ingresar";
+            btnIngresar.UseVisualStyleBackColor = false;
+            btnIngresar.Click += btnIngresar_Click;
+            // 
             // lblOrigenDeRespuesto
             // 
             lblOrigenDeRespuesto.AutoSize = true;
             lblOrigenDeRespuesto.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblOrigenDeRespuesto.Location = new Point(11, 128);
+            lblOrigenDeRespuesto.Location = new Point(20, 76);
             lblOrigenDeRespuesto.Name = "lblOrigenDeRespuesto";
             lblOrigenDeRespuesto.Size = new Size(171, 23);
             lblOrigenDeRespuesto.TabIndex = 1;
             lblOrigenDeRespuesto.Text = "Origen De Respuesto";
             // 
+            // btnCancelar
+            // 
+            btnCancelar.BackColor = Color.Gray;
+            btnCancelar.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnCancelar.ForeColor = SystemColors.ControlLightLight;
+            btnCancelar.Location = new Point(82, 300);
+            btnCancelar.Margin = new Padding(3, 4, 3, 4);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(106, 44);
+            btnCancelar.TabIndex = 10;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = false;
+            btnCancelar.Click += btnCancelar_Click;
+            // 
             // lblNumeroDelRespuesto
             // 
             lblNumeroDelRespuesto.AutoSize = true;
             lblNumeroDelRespuesto.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblNumeroDelRespuesto.Location = new Point(11, 187);
+            lblNumeroDelRespuesto.Location = new Point(20, 135);
             lblNumeroDelRespuesto.Name = "lblNumeroDelRespuesto";
             lblNumeroDelRespuesto.Size = new Size(186, 23);
             lblNumeroDelRespuesto.TabIndex = 2;
             lblNumeroDelRespuesto.Text = "Numero Del Respuesto";
             // 
+            // mtbNumeroRespuesto
+            // 
+            mtbNumeroRespuesto.Enabled = false;
+            mtbNumeroRespuesto.Location = new Point(214, 127);
+            mtbNumeroRespuesto.Margin = new Padding(3, 4, 3, 4);
+            mtbNumeroRespuesto.Mask = "999999";
+            mtbNumeroRespuesto.Name = "mtbNumeroRespuesto";
+            mtbNumeroRespuesto.Size = new Size(127, 27);
+            mtbNumeroRespuesto.TabIndex = 9;
+            mtbNumeroRespuesto.ValidatingType = typeof(int);
+            mtbNumeroRespuesto.MaskInputRejected += mtbNumeroRespuesto_MaskInputRejected;
+            // 
             // lblDescripcion
             // 
             lblDescripcion.AutoSize = true;
             lblDescripcion.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblDescripcion.Location = new Point(11, 229);
+            lblDescripcion.Location = new Point(20, 177);
             lblDescripcion.Name = "lblDescripcion";
             lblDescripcion.Size = new Size(98, 23);
             lblDescripcion.TabIndex = 3;
             lblDescripcion.Text = "Descripcion";
             // 
+            // txtDescripcion
+            // 
+            txtDescripcion.Enabled = false;
+            txtDescripcion.Location = new Point(214, 169);
+            txtDescripcion.Margin = new Padding(3, 4, 3, 4);
+            txtDescripcion.MaxLength = 50;
+            txtDescripcion.Name = "txtDescripcion";
+            txtDescripcion.Size = new Size(127, 27);
+            txtDescripcion.TabIndex = 8;
+            txtDescripcion.TextChanged += txtDescripcion_TextChanged;
+            // 
             // lblPrecioDelRespuesto
             // 
             lblPrecioDelRespuesto.AutoSize = true;
             lblPrecioDelRespuesto.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblPrecioDelRespuesto.Location = new Point(14, 285);
+            lblPrecioDelRespuesto.Location = new Point(20, 236);
             lblPrecioDelRespuesto.Name = "lblPrecioDelRespuesto";
             lblPrecioDelRespuesto.Size = new Size(57, 23);
             lblPrecioDelRespuesto.TabIndex = 4;
             lblPrecioDelRespuesto.Text = "Precio";
             // 
+            // cmbOrigen
+            // 
+            cmbOrigen.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbOrigen.Enabled = false;
+            cmbOrigen.FormattingEnabled = true;
+            cmbOrigen.Items.AddRange(new object[] { "(N) Nacional", "(I) Importado" });
+            cmbOrigen.Location = new Point(190, 68);
+            cmbOrigen.Margin = new Padding(3, 4, 3, 4);
+            cmbOrigen.Name = "cmbOrigen";
+            cmbOrigen.Size = new Size(151, 28);
+            cmbOrigen.TabIndex = 7;
+            cmbOrigen.SelectedIndexChanged += cmbOrigen_SelectedIndexChanged;
+            // 
             // lblPrecio
             // 
             lblPrecio.AutoSize = true;
-            lblPrecio.Location = new Point(181, 288);
+            lblPrecio.Location = new Point(190, 236);
             lblPrecio.Name = "lblPrecio";
             lblPrecio.Size = new Size(27, 20);
             lblPrecio.TabIndex = 5;
@@ -106,93 +208,132 @@
             cmbMarca.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbMarca.FormattingEnabled = true;
             cmbMarca.Items.AddRange(new object[] { "(P) Peugeot", "(F) Fiat", "(R) Renault" });
-            cmbMarca.Location = new Point(181, 68);
+            cmbMarca.Location = new Point(190, 16);
             cmbMarca.Margin = new Padding(3, 4, 3, 4);
             cmbMarca.Name = "cmbMarca";
             cmbMarca.Size = new Size(151, 28);
             cmbMarca.TabIndex = 6;
             cmbMarca.SelectedIndexChanged += cmbMarca_SelectedIndexChanged;
             // 
-            // cmbOrigen
+            // Consultas
             // 
-            cmbOrigen.DropDownStyle = ComboBoxStyle.DropDownList;
-            cmbOrigen.Enabled = false;
-            cmbOrigen.FormattingEnabled = true;
-            cmbOrigen.Items.AddRange(new object[] { "(N) Nacional", "(I) Importado" });
-            cmbOrigen.Location = new Point(181, 120);
-            cmbOrigen.Margin = new Padding(3, 4, 3, 4);
-            cmbOrigen.Name = "cmbOrigen";
-            cmbOrigen.Size = new Size(151, 28);
-            cmbOrigen.TabIndex = 7;
-            cmbOrigen.SelectedIndexChanged += cmbOrigen_SelectedIndexChanged;
+            Consultas.Controls.Add(btnConsultar);
+            Consultas.Controls.Add(lblMarcaDeRepuesto);
+            Consultas.Controls.Add(lstDatos);
+            Consultas.Controls.Add(lblOrigenDeRepuesto);
+            Consultas.Controls.Add(chkImportado);
+            Consultas.Controls.Add(cmbMarcaCta);
+            Consultas.Controls.Add(chkNacional);
+            Consultas.Location = new Point(4, 29);
+            Consultas.Name = "Consultas";
+            Consultas.Padding = new Padding(3);
+            Consultas.Size = new Size(383, 357);
+            Consultas.TabIndex = 1;
+            Consultas.Text = "consultas";
+            Consultas.UseVisualStyleBackColor = true;
             // 
-            // txtDescripcion
+            // btnConsultar
             // 
-            txtDescripcion.Enabled = false;
-            txtDescripcion.Location = new Point(205, 221);
-            txtDescripcion.Margin = new Padding(3, 4, 3, 4);
-            txtDescripcion.MaxLength = 50;
-            txtDescripcion.Name = "txtDescripcion";
-            txtDescripcion.Size = new Size(127, 27);
-            txtDescripcion.TabIndex = 8;
-            txtDescripcion.TextChanged += txtDescripcion_TextChanged;
+            btnConsultar.BackColor = Color.Gray;
+            btnConsultar.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnConsultar.ForeColor = SystemColors.ControlLightLight;
+            btnConsultar.Location = new Point(153, 296);
+            btnConsultar.Name = "btnConsultar";
+            btnConsultar.Size = new Size(115, 37);
+            btnConsultar.TabIndex = 19;
+            btnConsultar.Text = "Consultar";
+            btnConsultar.UseVisualStyleBackColor = false;
+            btnConsultar.Click += btnConsultar_Click;
             // 
-            // mtbNumeroRespuesto
+            // lblMarcaDeRepuesto
             // 
-            mtbNumeroRespuesto.Enabled = false;
-            mtbNumeroRespuesto.Location = new Point(205, 179);
-            mtbNumeroRespuesto.Margin = new Padding(3, 4, 3, 4);
-            mtbNumeroRespuesto.Mask = "999999";
-            mtbNumeroRespuesto.Name = "mtbNumeroRespuesto";
-            mtbNumeroRespuesto.Size = new Size(127, 27);
-            mtbNumeroRespuesto.TabIndex = 9;
-            mtbNumeroRespuesto.ValidatingType = typeof(int);
-            mtbNumeroRespuesto.MaskInputRejected += mtbNumeroRespuesto_MaskInputRejected;
+            lblMarcaDeRepuesto.AutoSize = true;
+            lblMarcaDeRepuesto.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblMarcaDeRepuesto.Location = new Point(18, 23);
+            lblMarcaDeRepuesto.Name = "lblMarcaDeRepuesto";
+            lblMarcaDeRepuesto.Size = new Size(57, 23);
+            lblMarcaDeRepuesto.TabIndex = 13;
+            lblMarcaDeRepuesto.Text = "Marca";
             // 
-            // btnCancelar
+            // lstDatos
             // 
-            btnCancelar.Location = new Point(73, 352);
-            btnCancelar.Margin = new Padding(3, 4, 3, 4);
-            btnCancelar.Name = "btnCancelar";
-            btnCancelar.Size = new Size(106, 44);
-            btnCancelar.TabIndex = 10;
-            btnCancelar.Text = "Cancelar";
-            btnCancelar.UseVisualStyleBackColor = true;
+            lstDatos.FormattingEnabled = true;
+            lstDatos.Location = new Point(18, 165);
+            lstDatos.Name = "lstDatos";
+            lstDatos.Size = new Size(250, 104);
+            lstDatos.TabIndex = 18;
             // 
-            // btnIngresar
+            // lblOrigenDeRepuesto
             // 
-            btnIngresar.Enabled = false;
-            btnIngresar.Location = new Point(197, 352);
-            btnIngresar.Margin = new Padding(3, 4, 3, 4);
-            btnIngresar.Name = "btnIngresar";
-            btnIngresar.Size = new Size(106, 44);
-            btnIngresar.TabIndex = 11;
-            btnIngresar.Text = "Ingresar";
-            btnIngresar.UseVisualStyleBackColor = true;
-            btnIngresar.Click += btnIngresar_Click;
+            lblOrigenDeRepuesto.AutoSize = true;
+            lblOrigenDeRepuesto.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblOrigenDeRepuesto.Location = new Point(18, 75);
+            lblOrigenDeRepuesto.Name = "lblOrigenDeRepuesto";
+            lblOrigenDeRepuesto.Size = new Size(62, 23);
+            lblOrigenDeRepuesto.TabIndex = 14;
+            lblOrigenDeRepuesto.Text = "Origen";
+            // 
+            // chkImportado
+            // 
+            chkImportado.AutoSize = true;
+            chkImportado.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            chkImportado.Location = new Point(117, 104);
+            chkImportado.Name = "chkImportado";
+            chkImportado.Size = new Size(112, 27);
+            chkImportado.TabIndex = 17;
+            chkImportado.TabStop = true;
+            chkImportado.Text = "Importado";
+            chkImportado.UseVisualStyleBackColor = true;
+            chkImportado.CheckedChanged += chkImportado_CheckedChanged;
+            // 
+            // cmbMarcaCta
+            // 
+            cmbMarcaCta.FormattingEnabled = true;
+            cmbMarcaCta.Items.AddRange(new object[] { "Fiat (F)", "Peugeot (P)", "Renault (R)" });
+            cmbMarcaCta.Location = new Point(117, 15);
+            cmbMarcaCta.Name = "cmbMarcaCta";
+            cmbMarcaCta.Size = new Size(151, 28);
+            cmbMarcaCta.TabIndex = 15;
+            cmbMarcaCta.SelectedIndexChanged += cmbMarcaCta_SelectedIndexChanged;
+            // 
+            // chkNacional
+            // 
+            chkNacional.AutoSize = true;
+            chkNacional.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            chkNacional.Location = new Point(117, 64);
+            chkNacional.Name = "chkNacional";
+            chkNacional.Size = new Size(98, 27);
+            chkNacional.TabIndex = 16;
+            chkNacional.TabStop = true;
+            chkNacional.Text = "Nacional";
+            chkNacional.UseVisualStyleBackColor = true;
+            chkNacional.CheckedChanged += chkNacional_CheckedChanged;
+            // 
+            // tpConsultar
+            // 
+            tpConsultar.Controls.Add(tpDatos);
+            tpConsultar.Controls.Add(Consultas);
+            tpConsultar.Location = new Point(12, 12);
+            tpConsultar.Name = "tpConsultar";
+            tpConsultar.SelectedIndex = 0;
+            tpConsultar.Size = new Size(391, 390);
+            tpConsultar.TabIndex = 12;
             // 
             // frmRegistro
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(362, 432);
-            Controls.Add(btnIngresar);
-            Controls.Add(btnCancelar);
-            Controls.Add(mtbNumeroRespuesto);
-            Controls.Add(txtDescripcion);
-            Controls.Add(cmbOrigen);
-            Controls.Add(cmbMarca);
-            Controls.Add(lblPrecio);
-            Controls.Add(lblPrecioDelRespuesto);
-            Controls.Add(lblDescripcion);
-            Controls.Add(lblNumeroDelRespuesto);
-            Controls.Add(lblOrigenDeRespuesto);
-            Controls.Add(lblMacaDeRespuesto);
+            ClientSize = new Size(415, 419);
+            Controls.Add(tpConsultar);
             Margin = new Padding(3, 4, 3, 4);
             Name = "frmRegistro";
             Text = "Venta De Respuestos";
+            tpDatos.ResumeLayout(false);
+            tpDatos.PerformLayout();
+            Consultas.ResumeLayout(false);
+            Consultas.PerformLayout();
+            tpConsultar.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -209,5 +350,14 @@
         private MaskedTextBox mtbNumeroRespuesto;
         private Button btnCancelar;
         private Button btnIngresar;
+        private TabControl tpConsultar;
+        private TabPage Consultas;
+        private Label lblMarcaDeRepuesto;
+        private Label lblOrigenDeRepuesto;
+        private ComboBox cmbMarcaCta;
+        private RadioButton chkNacional;
+        private RadioButton chkImportado;
+        private ListBox lstDatos;
+        private Button btnConsultar;
     }
 }
--- pryGiovannettiSP3/frmRegistro.cs
+++ pryGiovannettiSP3/frmRegistro.cs
@@ -1,3 +1,6 @@
+using System.Text.RegularExpressions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace pryGiovannettiSP3
 {
     public partial class frmRegistro : Form
@@ -7,13 +10,21 @@ namespace pryGiovannettiSP3
             InitializeComponent();
         }
         //Definicion de variables globales
-        float precio = 0;
+        float Precio = 0;
         public static string[] vecRespuesto = new string[100];
-        string marca;
-        string origen;
-        int numero;
+        string Marca;
+        string Origen;
+        int Numero;
         string Descripcion;
         int indice = 0;
+
+        //Declaración arrays
+        public static string[] vecRepuesto = new string[100];
+        public static string[] vecMarca = new string[100];
+        public static string[] vecOrigen = new string[100];
+        public static int[] vecNumero = new int[100];
+        public static string[] vecDescripcion = new string[100];
+        public static float[] vecPrecio = new float[100];
 
         private void cmbMarca_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -56,17 +67,127 @@ namespace pryGiovannettiSP3
             if (txtDescripcion.Text != "")
             {
                 btnIngresar.Enabled = true;
+                lblPrecio.Enabled = true;
             }
             else
             {
                 btnIngresar.Enabled = false;
+                lblPrecio.Enabled = false;
             }
         }
 
         private void btnIngresar_Click(object sender, EventArgs e)
         {
+            //Otorgamos valor a las variables 
+            Marca = cmbMarca.Text;
+            Origen = cmbOrigen.Text;
+            Numero = Convert.ToInt32(mtbNumeroRespuesto.Text.Trim());
+            Descripcion = txtDescripcion.Text;
+            Precio = Convert.ToInt32(lblPrecio.Text.Trim());
+
+            //Validamos que el array no este lleno
+            if (indice >= vecNumero.Length)
+            {
+                MessageBox.Show("El array se encuentra lleno, no se puede grabar más datos.", "Array lleno",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                btnIngresar.Enabled = false; //Deshabilitamos el btnIngresar para no agregar ningún dato nuevo
+                return; //detiene el el procedimiento para que no grabe ningún dato
+            }
+
+            //Validar que no haya un número repetido en el array
+            int i = 0;
+            bool existe = false;
+
+            while (i < indice && existe == false)   // recorre solo hasta la cantidad cargada y existe sea falso
+            {
+                if (vecNumero[i] == Numero)
+                {
+                    existe = true;
+                }
+                i++;
+            }
+            if (existe == true) //Validamos si sale del while por repetir número o no 
+            {
+                MessageBox.Show("Ya existe un repuesto con ese número.", "Número repetido",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                //Grabamos los datos
+                vecMarca[indice] = Marca;
+                vecOrigen[indice] = Origen;
+                vecNumero[indice] = Numero;
+                vecDescripcion[indice] = Descripcion;
+                vecPrecio[indice] = Precio;
+
+                indice++;
+            }
+
             vecRespuesto[indice] = "untexto";
             indice++;
         }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            cmbMarca.SelectedIndex = -1;
+            cmbOrigen.SelectedIndex = -1;
+            mtbNumeroRespuesto.Text = "";
+            txtDescripcion.Text = "";
+            lblPrecio.Text = "";
+            cmbMarca.Focus();
+
+
+
+        }
+
+        //Declaración variables consultas
+        int Indice = 0;
+        string MarcaSeleccionada = "";
+        string OrigenSeleccionado = "";
+
+        private void cmbMarcaCta_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbMarcaCta.SelectedIndex != -1)
+            {
+                chkNacional.Enabled = true;
+                chkImportado.Enabled = true;
+            }
+
+        }
+
+        private void chkNacional_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkNacional.Checked == true)
+            {
+                OrigenSeleccionado = "(N) Nacional"; //Otorgamos valor a la variable OrigenSeleccionado
+                btnConsultar.Enabled = true;
+            }
+            else
+            {
+                btnConsultar.Enabled = false;
+            }
+        }
+
+        private void chkImportado_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkImportado.Checked == true)
+            {
+                btnConsultar.Enabled = true;
+                OrigenSeleccionado = "(I) Importado";
+            }
+            else
+            {
+                btnConsultar.Enabled = false;
+            }
+        }
+        //se utilizara para acumular los valores y mostrarlos
+        string Resultado = "";
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            Resultado  = ""; //Limpio las variables
+            MarcaSeleccionada = cmbMarca.Text;
+
+        }
     }
 }
+
